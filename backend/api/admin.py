9fdd217968102ede9bf0be871df1efe7015d520e
Benjamin Lau <ben_lau93@hotmail.com>
2022-05-02 10:15:53 +0800

    removed data model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import DataModel, BankModel, CPFModel, InvestmentModel
+from .models import BankModel, CPFModel, InvestmentModel
 
 # Register your models here.
-admin.site.register(DataModel)
 admin.site.register(BankModel)
 admin.site.register(CPFModel)
 admin.site.register(InvestmentModel)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class DataModel(models.Model):
-    DATE = models.DateField(blank=False)
-    ASSET_TYPE = models.CharField(max_length=20, blank=False)
-    VALUE = models.FloatField(blank=False)
-
 class CPFModel(models.Model):
     ID = models.IntegerField(primary_key=True)
     DATE = models.DateField(blank=False)

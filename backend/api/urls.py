dc27d851241ee6f382fd6e416208b7cfa4aaaaab
BENJAMIN LAU <ben_lau93@hotmail.com>
2022-05-21 11:22:34 +0800

    added debt view to url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ExtractInvestmentViews, PDFExtractionViews, CPFView, BankView, InvestmentView
+from .views import ExtractInvestmentViews, PDFExtractionViews, CPFView, BankView, InvestmentView, DebtView
 
 urlpatterns = [
     path("extract-investment", ExtractInvestmentViews.as_view()),
     path("extract", PDFExtractionViews.as_view()),
     path("cpf",CPFView.as_view()),
     path("bank",BankView.as_view()),
-    path("investment",InvestmentView.as_view())
+    path("investment",InvestmentView.as_view()),
+    path("debt", DebtView.as_view())
 ]

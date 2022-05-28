5a879cff45220f647ec858a03cd9d4e62c6dc58c
BENJAMIN LAU <ben_lau93@hotmail.com>
2022-05-28 11:44:41 +0800

    added historical extraction url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ExtractInvestmentViews, PDFExtractionViews, CPFView, BankView, InvestmentView, DebtView
+from .views import ExtractInvestmentViews, PDFExtractionViews, CPFView, BankView, InvestmentView, DebtView, PDFExtractionHistoricalViews
 
 urlpatterns = [
     path("extract-investment", ExtractInvestmentViews.as_view()),
     path("extract", PDFExtractionViews.as_view()),
+    path("historical", PDFExtractionHistoricalViews.as_view())
     path("cpf",CPFView.as_view()),
     path("bank",BankView.as_view()),
     path("investment",InvestmentView.as_view()),

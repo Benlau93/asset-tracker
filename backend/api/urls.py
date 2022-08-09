0d24294a0a4026e33efa6872afab0177fc888437
Benjamin Lau <ben_lau93@hotmail.com>
2022-08-09 15:58:36 +0800

    added tax url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ExtractInvestmentView, PDFExtractionView, CPFView, BankView, InvestmentView, DebtView, HistoricalExtractionView
+from .views import ExtractInvestmentView, PDFExtractionView, CPFView, BankView, InvestmentView, DebtView, HistoricalExtractionView, TaxView, ReliefView
 
 urlpatterns = [
     path("extract-investment", ExtractInvestmentView.as_view()),
@@ -8,5 +8,7 @@ urlpatterns = [
     path("cpf",CPFView.as_view()),
     path("bank",BankView.as_view()),
     path("investment",InvestmentView.as_view()),
-    path("debt", DebtView.as_view())
+    path("debt", DebtView.as_view()),
+    path("tax",TaxView.as_view()),
+    path("relief", ReliefView.as_view())
 ]

64f92fbac6a170673d454f7789fbeefbcb223ac4
Benjamin Lau <ben_lau93@hotmail.com>
2022-08-09 18:47:13 +0800

    initialize relief table

--- apps/tax.py
+++ apps/tax.py
@@ -1,6 +1,7 @@
 from app import app
 from dash import html
 from dash import dcc
+from dash import dash_table
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -8,12 +9,100 @@ import pandas as pd
 from dash.dependencies import Input, Output, State
 from app import app
 from datetime import date
+from dash.dash_table.Format import Format,Scheme
 
 # define template used
 TEMPLATE = "plotly_white"
 
 # define variables
 YEAR = date.today().year
+
+
+# kpi
+def generate_indicators(df):
+    # get total income for current year
+    total_income = df["INCOME"].iloc[0]
+
+    main_kpi_fig = go.Figure()
+    main_kpi_fig.add_trace(
+        go.Indicator(mode="number",
+                    value = total_income,
+                    title = "Total Annual Income",
+                    number = dict(valueformat="$,.0f")
+                    )
+    )
+
+    main_kpi_fig.update_layout(
+        height = 250,
+        template = TEMPLATE
+    )
+
+    # get tax payable
+    tax_annual = df["TAX_YEAR"].iloc[0]
+    tax_month = df["TAX_MONTH"].iloc[0]
+
+    kpi_fig = go.Figure()
+    kpi_fig.add_trace(
+        go.Indicator(mode="number",
+                    value = tax_annual,
+                    title = "Tax Payable (Annual)",
+                    number = dict(valueformat="$,.02f"),
+                    domain = {"row":1, "column":0}
+                    )
+                    
+    )
+
+    kpi_fig.add_trace(
+        go.Indicator(mode="number",
+                    value = tax_month,
+                    title = "Tax Payable (Monthly)",
+                    number = dict(valueformat="$,.02f"),
+                    domain = {"row":2, "column":1}
+                    )
+                    
+    )
+
+    kpi_fig.update_layout(
+        grid = {"rows":1, "columns":2, "pattern":"independent"},
+        height = 250,
+        template = TEMPLATE
+    )
+
+    return main_kpi_fig, kpi_fig
+
+
+# relief table
+def generate_relief_table(df):
+    df = df.drop(["ID","YEAR"], axis=1).copy()
+    df = df.sort_values("RELIEF")
+    df.columns = df.columns.str.capitalize()
+
+    money = dash_table.FormatTemplate.money(2)
+
+    # table
+    table_fig = dash_table.DataTable(
+        id="tax-table",
+        columns = [
+            dict(id="Relief", name="Relief"),
+            dict(id="Value", name="Value", type="numeric", format=money),
+        ],
+
+        data=df.to_dict('records'),
+        sort_action="native",
+        # row_selectable='single',
+        style_cell={
+        'height': 'auto',
+        'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
+        'whiteSpace': 'normal'},
+        style_table={'overflowX': 'scroll'},
+        style_as_list_view=True,
+        page_action="native",
+        page_current= 0,
+        page_size= 10,
+    )
+
+    return table_fig
+
 
 layout = html.Div([
     dbc.Container([
@@ -27,26 +116,35 @@ layout = html.Div([
             )], width=2),
         ], align="center", justify="center", style={"margin-top":10}),
         dbc.Row([
-            dbc.Card(html.H3("Income Tax", className="text-center text-primary bg-light"), body=True, color="light")
+            dbc.Card(html.H3("Income Tax Assessment", className="text-center text-primary bg-light"), body=True, color="light")
         ], style={"margin-top":20}),
         dbc.Row([
-            dbc.Col(html.Div(html.H2(id="tax-title"), style={"margin-top":80}), width={"size":6, "offset":2})
+            dbc.Col([dcc.Graph(id="tax-main-kpi")], width=6)
         ], justify="center"),
         dbc.Row([
-            dbc.Col(html.Div(html.Span("Total Annual Income:"), style={"margin-top":40, "font-size":32 }), width={"size":4, "offset":0}),
-            dbc.Col(html.Div(html.Span("$100,000"), style={"margin-top":40, "font-size":32 }), width={"size":3, "offset":0})
-        ]),
+            dbc.Col([dcc.Graph(id="tax-sub-kpi")], width=10)
+        ], justify="center"),
+        dbc.Row([
+            dbc.Card(html.H3("Breakdown of Reliefs", className="text-center text-primary bg-light"), body=True, color="light")
+        ], style={"margin-top":20}),
+        dbc.Row([
+            dbc.Col(id="tax-table",width={"size":6}, style={"margin-top":50})
+        ], align="center", justify="center"),
     ])
 ])
 
 
 @app.callback(
-    Output(component_id="tax-title", component_property="children"),
+    Output(component_id="tax-main-kpi", component_property="figure"),
+    Output(component_id="tax-sub-kpi", component_property="figure"),
+    Output(component_id="tax-table", component_property="children"),
     Input(component_id="tax-year", component_property="value"),
     State(component_id="tax-store", component_property="data"),
     State(component_id="relief-store", component_property="data")
 )
 def update_figures(year, tax_df, relief):
+    # convert to int
+    year = int(year)
     # get tax for the year
     tax_df = pd.DataFrame(tax_df)
     tax_df = tax_df[tax_df["YEAR"]==year].copy()
@@ -54,5 +152,9 @@ def update_figures(year, tax_df, relief):
     # get relief for the year
     relief = pd.DataFrame(relief)
     relief = relief[relief["YEAR"]==year].copy()
+    print(relief)
+    # generate chart
+    main_kpi_fig, kpi_fig = generate_indicators(tax_df)
+    table_fig = generate_relief_table(relief)
 
-    return f"{year} TAX ASSESSMENT"
+    return main_kpi_fig, kpi_fig, table_fig

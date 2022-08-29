16b0f0760bdcdefee7613a7346bcb7c7d5844f08
Benjamin Lau <ben_lau93@hotmail.com>
2022-08-29 18:38:11 +0800

    change title

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import dash_bootstrap_components as dbc
 # initialize app
 external_stylesheets = [dbc.themes.FLATLY]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
-
+app.title = "Asset Tracking"
 server = app.server
